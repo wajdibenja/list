b288bd3ec81a2a45ef08fa361497b5f2fbeddaa2
Wajdi Ben Nja <khalladiaymen68@gmail.com>
2021-11-02 10:19:41 +0100

    Initial commit

---
+++ app/src/main/java/com/wejdibennja/affirmations/data/Datasource.kt
@@ -0,0 +1,24 @@
+package com.wejdibennja.affirmations.data
+
+
+import com.wejdibennja.affirmations.R
+import com.wejdibennja.affirmations.model.Affirmation
+
+
+class Datasource {
+
+    fun loadAffirmations(): List<Affirmation> {
+        return listOf<Affirmation>(
+            Affirmation(R.string.affirmation1),
+            Affirmation(R.string.affirmation2),
+            Affirmation(R.string.affirmation3),
+            Affirmation(R.string.affirmation4),
+            Affirmation(R.string.affirmation5),
+            Affirmation(R.string.affirmation6),
+            Affirmation(R.string.affirmation7),
+            Affirmation(R.string.affirmation8),
+            Affirmation(R.string.affirmation9),
+            Affirmation(R.string.affirmation10)
+        )
+    }
+}
---
+++ app/src/main/java/com/wejdibennja/affirmations/model/Affirmation.kt
@@ -0,0 +1,3 @@
+package com.wejdibennja.affirmations.model
+
+data class Affirmation(val stringResourceId: Int)
